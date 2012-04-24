3e247fea89eb4fdafae0f0b47824c62bfa0a10ba
Caleb Perkins <frostmourne@gmail.com>
2012-04-24 12:34:25 -0400

    SlowWordCount configurable

--- src/demos/SlowWordCount.java
+++ src/demos/SlowWordCount.java
@@ -5,19 +5,21 @@ import c2c.api.OutputCollector;
 import c2c.api.Reducer;
 
 /**
- * Simulates a slow mapping operation.
+ * Simulates a slow mapping and reducing operation.
  * @author Caleb Perkins
  *
  */
 public class SlowWordCount implements Reducer, Mapper {
+	private static final int WAIT_SECONDS = 20;
 
 	@Override
 	public void map(String key, String value, OutputCollector collector) {
-		long t0, t1; // busy-wait for 3 seconds
+		long t0, t1; // busy-wait for 20 seconds
 		t0 = System.currentTimeMillis();
 		do {
 			t1 = System.currentTimeMillis();
-		} while ((t1 - t0) < 20 * 1000);
+		} while ((t1 - t0) < WAIT_SECONDS * 1000);
+		
 		String[] words = value.split("\\s+");
 		for (String w : words) {
 			collector.collect(w, "1");
@@ -27,6 +29,12 @@ public class SlowWordCount implements Reducer, Mapper {
 	@Override
 	public void reduce(String key, Iterable<String> values,
 			OutputCollector collector) {
+		long t0, t1; // busy-wait for 20 seconds
+		t0 = System.currentTimeMillis();
+		do {
+			t1 = System.currentTimeMillis();
+		} while ((t1 - t0) < WAIT_SECONDS * 1000);
+		
 		int count = 0;
 		for (String s : values) {
 			count += Integer.parseInt(s);
